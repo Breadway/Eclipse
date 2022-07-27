61d52909e80381cf43160b08d024aac91a132134
Breadway <108389940+Breadway@users.noreply.github.com>
2022-07-28 00:39:57 +0800

    Dev

--- EclipseMainScript.lua
+++ EclipseMainScript.lua
@@ -23,49 +23,19 @@ part.CanCollide = false
 part.Anchored = true
 
 local KillAura = Combat:NewToggle("KillAura", "Automatically Attack Players", function(state)
-    if state then
-	KillToggled = true
-	repeat
-					
-		wait(0.3)
-       	 	for i,v in pairs(game.Players:GetPlayers()) do
-				local vchr = v.Character
-				if vchr and v~=Player and v.Team~=Player.Team and Character and Character.Humanoid.Health>0 then
-					local vhum = vchr:FindFirstChild("Humanoid")
-					local vroot = vchr:FindFirstChild("HumanoidRootPart")
-					if vhum and vroot and vhum.Health>0 and (vroot.Position-Character.HumanoidRootPart.Position).magnitude<=Range then
-						RemoteFolder.SwordHit:InvokeServer({entityInstance=vchr,weapon=Character.HandInvItem.Value})
-					end
-				end
-		end
-	until not state	
-    else
-	KillToggled = false
-    end
+    
 end)
 
 Combat:NewToggle("Velocity", "No KnockBack", function(state)
-    if state then
-        print("Toggle On")
-    else
-        print("Toggle Off")
-    end
+    
 end)
 
 Combat:NewToggle("Autoclicker", "Clicks when mouse button is held down", function(state)
-    if state then
-        print("Toggle On")
-    else
-        print("Toggle Off")
-    end
+ 
 end)
 
 Combat:NewToggle("Aim Assist", "Better Aim Because Ur Bad", function(state)
-    if state then
-        print("Toggle On")
-    else
-        print("Toggle Off")
-    end
+   
 end)
 
 -- Player Tab --
@@ -82,58 +52,20 @@ local BlatantTab = Window:NewTab("Blatant")
 local Blatant = BlatantTab:NewSection("Blatant")
 
 Blatant:NewToggle("NoFall", "Take No Fall Damage", function(state)
-    if state then
-        print("Toggle On")
-    else
-        print("Toggle Off")
-    end
+   
+		
 end)
 
 Blatant:NewToggle("HighJump", "Jump High", function(state)
-    if state then
-        print("Toggle On")
-    else
-        print("Toggle Off")
-    end
+    
 end)
 
 Blatant:NewToggle("LongJump", "Fly", function(state)
-    if state then
-        print("Toggle On")
-    else
-        print("Toggle Off")
-    end
+   
 end)
 
 Blatant:NewToggle("NoSlowdown", "Dont Slowdown When using Bows, Apples etc", function(state)
-    if state then
-        print("Toggle On")
-    else
-        print("Toggle Off")
-    end
-end)
-
-local GravityToggle = Blatant:NewToggle("Gravity", "Changes Workspace Gravity", function(state)
-    if state then
-        GravityToggled = True
-	game.workspace.Gravity = Gravity
-    else
-       GravityToggled = false
-	Gravity = 196.2
-	game.workspace.Gravity = Gravity
-    end
-    game.workspace.Gravity = Gravity
-end)
-
-Blatant:NewSlider("Gravity", " ", 500, -500, function(s) -- 500 (MaxValue) | 0 (MinValue)
-    if GravityToggled then
-		Gravity = s
-		return	GravityToggle
-	else
-		Gravity = 196.2
-		return GravityToggle
-	end
-	game.workspace.Gravity = Gravity
+    
 end)
 
 -- World Tab --
@@ -173,7 +105,6 @@ KeybindsP:NewKeybind("Toggle Speed", "Changes Player Speed, 23 Is Reccomended", 
 		SpeedToggle:UpdateToggle("Toggle Off")
 	elseif not SpeedToggled then
 		SpeedToggle:UpdateToggle("Toggle On")
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
 	end
 end)
 
