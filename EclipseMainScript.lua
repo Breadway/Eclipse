877ebf9054268cde9141b33b029de33b83f250fe
Breadway <108389940+Breadway@users.noreply.github.com>
2022-07-27 16:38:24 +0800

    dev

--- EclipseMainScript.lua
+++ EclipseMainScript.lua
@@ -1,8 +1,8 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Inhaler X", "GrapeTheme")
+local Window = Library.CreateLib("Eclipse", "GrapeTheme")
 
 -- Variables --
-local Var = {
+
 local KillToggled = false
 local SpeedToggled = false
 local GravityToggled = false
@@ -11,41 +11,20 @@ local Velocity = false
 local NoFall = false
 local Gravity = 196.2
 local speed = 16
-}
-
--- Tab & Section Creation --
-
-local CombatTab = Window:NewTab("Combat")
-local Combat =CombatTab:NewSection("Combat")
-
-local PlayerTab = Window:NewTab("Player")
-local Player = PlayerTab:NewSection("Player")
-
-local BlatantTab = Window:NewTab("Blatant")
-local Blatant = BlatantTab:NewSection("Blatant")
-
-local WorldTab = Window:NewTab("World")
-local World = WorldTab:NewSection("World")
-
-local RenderTab = Window:NewTab("Render")
-local Render = RenderTab:NewSection("Render")
-
-local KeybindsTab = Window:NewTab("Keybinds")
-local KeybindsTG = RenderTab:NewSection("ToggleGui")
-local KeybindsC = RenderTab:NewSection("Combat")
-local KeybindsP = RenderTab:NewSection("Player")
-local KeybindsB = RenderTab:NewSection("Blatant")
-local KeybindsW = RenderTab:NewSection("World")
-local KeybindsR = RenderTab:NewSection("Render")
-
-local CreditsTab = Window:NewTab("Credits")
-local Credits = OtherTab:NewSection("Credits")
 
 -- Combat Tab --
 
+local CombatTab = Window:NewTab("Combat")
+local Combat = CombatTab:NewSection("Combat")
+
+local part = instance.new("Part")
+part.Position = game.Players.Amazing_Hac3000.Position
+part.CanCollide = false
+part.Anchored = true
+
 local KillAura = Combat:NewToggle("KillAura", "Automatically Attack Players", function(state)
     if state then
-	Var.KillToggled = true
+	KillToggled = true
 	repeat
 					
 		wait(0.3)
@@ -61,7 +40,7 @@ local KillAura = Combat:NewToggle("KillAura", "Automatically Attack Players", fu
 		end
 	until not state	
     else
-	Var.KillToggled = false
+	KillToggled = false
     end
 end)
 
@@ -91,30 +70,36 @@ end)
 
 -- Player Tab --
 
+local PlayerTab = Window:NewTab("Player")
+local Player = PlayerTab:NewSection("Player")
+
 local SpeedToggle = Player:NewToggle("Speed", "ToggleInfo", function(state)
     if state then
-        Var.SpeedToggled = True
+       SpeedToggled = True
 	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
     else
-        Var.SpeedToggled = false
-	Var.speed = 16
-	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Var.speed
+       SpeedToggled = false
+	speed = 16
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
     end
 end)
 
 Player:NewSlider("Speed", "Changes Player Speed, 23 Is Reccomended", 50, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
-	if Var.SpeedToggled then
-		Var.speed = s
+	if SpeedToggled then
+		speed = s
 		return	SpeedToggle
 	else
-		Var.speed = 16
+		speed = 16
 		return SpeedToggle
 	end
-	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Var.speed
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
 end)
 
 
 -- Blatant Tab --
+
+local BlatantTab = Window:NewTab("Blatant")
+local Blatant = BlatantTab:NewSection("Blatant")
 
 Blatant:NewToggle("NoFall", "Take No Fall Damage", function(state)
     if state then
@@ -150,55 +135,72 @@ end)
 
 local GravityToggle = Blatant:NewToggle("Gravity", "Changes Workspace Gravity", function(state)
     if state then
-        Var.GravityToggled = True
-	game.workspace.Gravity = Var.Gravity
+        GravityToggled = True
+	game.workspace.Gravity = Gravity
     else
-        Var.GravityToggled = false
-	Var.Gravity = 196.2
+       GravityToggled = false
+	Gravity = 196.2
 	game.workspace.Gravity = Gravity
     end
     game.workspace.Gravity = Gravity
 end)
 
 Blatant:NewSlider("Gravity", " ", 500, -500, function(s) -- 500 (MaxValue) | 0 (MinValue)
-    if Var.GravityToggled then
-		Var.Gravity = s
+    if GravityToggled then
+		Gravity = s
 		return	GravityToggle
 	else
-		Var.Gravity = 196.2
+		Gravity = 196.2
 		return GravityToggle
 	end
-	game.workspace.Gravity = Var.Gravity
+	game.workspace.Gravity = Gravity
 end)
 
 -- World Tab --
 
+local WorldTab = Window:NewTab("World")
+local World = WorldTab:NewSection("World")
+
 -- Render Tab --
 
+local RenderTab = Window:NewTab("Render")
+local Render = RenderTab:NewSection("Render")
+
 -- Keybinds Tab --
+
+local KeybindsTab = Window:NewTab("Keybinds")
+local KeybindsTG = KeybindsTab:NewSection("ToggleGui")
+local KeybindsC = KeybindsTab:NewSection("Combat")
+local KeybindsP = KeybindsTab:NewSection("Player")
+local KeybindsB = KeybindsTab:NewSection("Blatant")
+local KeybindsW = KeybindsTab:NewSection("World")
+local KeybindsR = KeybindsTab:NewSection("Render")
 
 KeybindsTG:NewKeybind("Toggle Gui", "Toggles The Inhaler X Gui", Enum.KeyCode.RightShift, function()
 	Library:ToggleUI()
 end)
 
 KeybindsC:NewKeybind("Toggle KillAura", "Automatically Attack Players", Enum.KeyCode.R, function()
-	if Var.KillToggled then
+	if KillToggled then
 		KillAura:UpdateToggle("Toggle Off")
-	elseif not Var.KillToggled
+	elseif not KillToggled then
 		KillAura:UpdateToggle("Toggle On")
 	end
 end) 
 
 KeybindsP:NewKeybind("Toggle Speed", "Changes Player Speed, 23 Is Reccomended", Enum.KeyCode.V, function()
-	if Var.SpeedToggled then
+	if SpeedToggled then
 		SpeedToggle:UpdateToggle("Toggle Off")
-	elseif not Var.SpeedToggled then
+	elseif not SpeedToggled then
 		SpeedToggle:UpdateToggle("Toggle On")
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Var.speed
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
 	end
 end)
 
 -- Credits Tab --
+
+local CreditsTab = Window:NewTab("Credits")
+local Credits = CreditsTab:NewSection("Credits")
 
 Credits:NewLabel("Owner - Breadway (@Breadway#2678)")
 Credits:NewLabel("Co-Owner - Emi H (@emirtube11)")
