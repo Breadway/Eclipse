f51a859cc539f258c4e10866a5a2f7d8e9867918
Breadway <108389940+Breadway@users.noreply.github.com>
2022-07-27 14:30:16 +0800

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,13 @@
+local Variables{
+  
+local KillToggled = false,
+local SpeedToggled = false,
+local GravityToggled = false,
+local Autoclicker = false,
+local Velocity = false,
+local NoFall = false,
+local Gravity = 196.2,
+local speed = 16,
+
+
+}
