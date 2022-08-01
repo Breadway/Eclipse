ccb0c46c3d87ff4cf0e80cc9f19c9e81c8892889
Breadway <108389940+Breadway@users.noreply.github.com>
2022-08-01 16:27:45 +0800

    Add files via upload

--- NewGuiLibrary.lua
+++ NewGuiLibrary.lua
@@ -2,136 +2,141 @@ local Lib = {}
 
 function Lib:CreateMain()
 	local ScreenGui = Instance.new("ScreenGui")
-	local Frame = Instance.new("Frame")
-	local Combat1 = Instance.new("Frame")
-	local UIListLayout = Instance.new("UIListLayout")
-	local Blatant = Instance.new("TextLabel")
-	local Blatant_2 = Instance.new("Frame")
-	local UIListLayout_2 = Instance.new("UIListLayout")
-	local TextLabel = Instance.new("TextLabel")
-	local Render = Instance.new("Frame")
-	local UIListLayout_3 = Instance.new("UIListLayout")
-	local TextLabel_2 = Instance.new("TextLabel")
-	local Utility = Instance.new("Frame")
-	local UIListLayout_4 = Instance.new("UIListLayout")
-	local TextLabel_3 = Instance.new("TextLabel")
-	local World = Instance.new("Frame")
-	local UIListLayout_5 = Instance.new("UIListLayout")
-	local TextLabel_4 = Instance.new("TextLabel")
-	local UIListLayout_6 = Instance.new("UIListLayout")
-	
-	--Properties:
-	
-	ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-	ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-	
-	Frame.Parent = ScreenGui
-	Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	Frame.BackgroundTransparency = 1.000
-	Frame.Position = UDim2.new(0.0187546778, 0, 0.0171358772, 0)
-	Frame.Size = UDim2.new(0, 1283, 0, 788)
-	
-	Combat1.Name = "Combat"
-	Combat1.Parent = Frame
-	Combat1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	Combat1.Size = UDim2.new(0, 161, 0, 474)
-	
-	UIListLayout.Parent = Combat
-	UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-	
-	Blatant.Name = "Blatant"
-	Blatant.Parent = Combat
-	Blatant.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	Blatant.Size = UDim2.new(0, 161, 0, 50)
-	Blatant.Font = Enum.Font.SourceSansBold
-	Blatant.TextColor3 = Color3.fromRGB(0, 0, 0)
-	Blatant.TextScaled = true
-	Blatant.TextSize = 14.000
-	Blatant.TextWrapped = true
+local Frame = Instance.new("Frame")
+local Combat = Instance.new("Frame")
+local UIListLayout = Instance.new("UIListLayout")
+local CombatLabel = Instance.new("TextLabel")
+local Blatant1 = Instance.new("Frame")
+local UIListLayout_2 = Instance.new("UIListLayout")
+local BlatantLabel = Instance.new("TextLabel")
+local Render = Instance.new("Frame")
+local UIListLayout_3 = Instance.new("UIListLayout")
+local RenderLabel = Instance.new("TextLabel")
+local Utility = Instance.new("Frame")
+local UIListLayout_4 = Instance.new("UIListLayout")
+local UtilityLabel = Instance.new("TextLabel")
+local World = Instance.new("Frame")
+local UIListLayout_5 = Instance.new("UIListLayout")
+local WorldLabel = Instance.new("TextLabel")
+local UIListLayout_6 = Instance.new("UIListLayout")
 
-	Blatant_2.Name = "Blatant"
-	Blatant_2.Parent = Frame
-	Blatant_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	Blatant_2.Size = UDim2.new(0, 161, 0, 474)
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.Position = UDim2.new(0.0187546778, 0, 0.0171358772, 0)
+Frame.Size = UDim2.new(0, 1283, 0, 788)
+
+Combat.Name = "Combat"
+Combat.Parent = Frame
+Combat.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Combat.Size = UDim2.new(0, 161, 0, 474)
+
+UIListLayout.Parent = Combat
+UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+
+CombatLabel.Name = "CombatLabel"
+CombatLabel.Parent = Combat
+CombatLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+CombatLabel.Size = UDim2.new(0, 161, 0, 50)
+CombatLabel.Font = Enum.Font.SourceSansBold
+CombatLabel.Text = "Combat"
+CombatLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+CombatLabel.TextScaled = true
+CombatLabel.TextSize = 14.000
+CombatLabel.TextWrapped = true
+
+Blatant1.Name = "Blatant1"
+Blatant1.Parent = Frame
+Blatant1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Blatant1.Size = UDim2.new(0, 161, 0, 474)
+
+UIListLayout_2.Parent = Blatant1
+UIListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
+
+BlatantLabel.Name = "BlatantLabel"
+BlatantLabel.Parent = Blatant1
+BlatantLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+BlatantLabel.Size = UDim2.new(0, 161, 0, 50)
+BlatantLabel.Font = Enum.Font.SourceSansBold
+BlatantLabel.Text = "Blatant"
+BlatantLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+BlatantLabel.TextScaled = true
+BlatantLabel.TextSize = 14.000
+BlatantLabel.TextWrapped = true
+
+Render.Name = "Render"
+Render.Parent = Frame
+Render.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Render.Size = UDim2.new(0, 161, 0, 474)
+
+UIListLayout_3.Parent = Render
+UIListLayout_3.SortOrder = Enum.SortOrder.LayoutOrder
+
+RenderLabel.Name = "RenderLabel"
+RenderLabel.Parent = Render
+RenderLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+RenderLabel.Size = UDim2.new(0, 161, 0, 50)
+RenderLabel.Font = Enum.Font.SourceSansBold
+RenderLabel.Text = "Render"
+RenderLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+RenderLabel.TextScaled = true
+RenderLabel.TextSize = 14.000
+RenderLabel.TextWrapped = true
+
+Utility.Name = "Utility"
+Utility.Parent = Frame
+Utility.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Utility.Size = UDim2.new(0, 161, 0, 474)
+
+UIListLayout_4.Parent = Utility
+UIListLayout_4.SortOrder = Enum.SortOrder.LayoutOrder
+
+UtilityLabel.Name = "UtilityLabel"
+UtilityLabel.Parent = Utility
+UtilityLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+UtilityLabel.Size = UDim2.new(0, 161, 0, 50)
+UtilityLabel.Font = Enum.Font.SourceSansBold
+UtilityLabel.Text = "Utility"
+UtilityLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+UtilityLabel.TextScaled = true
+UtilityLabel.TextSize = 14.000
+UtilityLabel.TextWrapped = true
+
+World.Name = "World"
+World.Parent = Frame
+World.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+World.Size = UDim2.new(0, 161, 0, 474)
+
+UIListLayout_5.Parent = World
+UIListLayout_5.SortOrder = Enum.SortOrder.LayoutOrder
+
+WorldLabel.Name = "WorldLabel"
+WorldLabel.Parent = World
+WorldLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+WorldLabel.Size = UDim2.new(0, 161, 0, 50)
+WorldLabel.Font = Enum.Font.SourceSansBold
+WorldLabel.Text = "World"
+WorldLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+WorldLabel.TextScaled = true
+WorldLabel.TextSize = 14.000
+WorldLabel.TextWrapped = true
+
+UIListLayout_6.Parent = Frame
+UIListLayout_6.FillDirection = Enum.FillDirection.Horizontal
+UIListLayout_6.SortOrder = Enum.SortOrder.LayoutOrder
+UIListLayout_6.Padding = UDim.new(0, 100)
 	
-	UIListLayout_2.Parent = Blatant_2
-	UIListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
+	local Blatant = {}
 	
-	TextLabel.Parent = Blatant_2
-	TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	TextLabel.Size = UDim2.new(0, 161, 0, 50)
-	TextLabel.Font = Enum.Font.SourceSansBold
-	TextLabel.Text = "Combat"
-	TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-	TextLabel.TextScaled = true
-	TextLabel.TextSize = 14.000
-	TextLabel.TextWrapped = true
-	
-	Render.Name = "Render"
-	Render.Parent = Frame
-	Render.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	Render.Size = UDim2.new(0, 161, 0, 474)
-	
-	UIListLayout_3.Parent = Render
-	UIListLayout_3.SortOrder = Enum.SortOrder.LayoutOrder
-	
-	TextLabel_2.Parent = Render
-	TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	TextLabel_2.Size = UDim2.new(0, 161, 0, 50)
-	TextLabel_2.Font = Enum.Font.SourceSansBold
-	TextLabel_2.Text = "Render"
-	TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
-	TextLabel_2.TextScaled = true
-	TextLabel_2.TextSize = 14.000
-	TextLabel_2.TextWrapped = true
-	
-	Utility.Name = "Utility"
-	Utility.Parent = Frame
-	Utility.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	Utility.Size = UDim2.new(0, 161, 0, 474)
-	
-	UIListLayout_4.Parent = Utility
-	UIListLayout_4.SortOrder = Enum.SortOrder.LayoutOrder
-	
-	TextLabel_3.Parent = Utility
-	TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	TextLabel_3.Size = UDim2.new(0, 161, 0, 50)
-	TextLabel_3.Font = Enum.Font.SourceSansBold
-	TextLabel_3.Text = "Utility"
-	TextLabel_3.TextColor3 = Color3.fromRGB(0, 0, 0)
-	TextLabel_3.TextScaled = true
-	TextLabel_3.TextSize = 14.000
-	TextLabel_3.TextWrapped = true
-	
-	World.Name = "World"
-	World.Parent = Frame
-	World.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	World.Size = UDim2.new(0, 161, 0, 474)
-	
-	UIListLayout_5.Parent = World
-	UIListLayout_5.SortOrder = Enum.SortOrder.LayoutOrder
-	
-	TextLabel_4.Parent = World
-	TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	TextLabel_4.Size = UDim2.new(0, 161, 0, 50)
-	TextLabel_4.Font = Enum.Font.SourceSansBold
-	TextLabel_4.Text = "World"
-	TextLabel_4.TextColor3 = Color3.fromRGB(0, 0, 0)
-	TextLabel_4.TextScaled = true
-	TextLabel_4.TextSize = 14.000
-	TextLabel_4.TextWrapped = true
-	
-	UIListLayout_6.Parent = Frame
-	UIListLayout_6.FillDirection = Enum.FillDirection.Horizontal
-	UIListLayout_6.SortOrder = Enum.SortOrder.LayoutOrder
-	UIListLayout_6.Padding = UDim.new(0, 100)
-	
-	local Combat = {}
-	
-	function Combat:AddButton(Name)
+	function Blatant:AddButton(Name)
 		local TextButton = Instance.new("TextButton")
 
-		TextButton.Parent = Combat1
+		TextButton.Parent = Blatant_2
 		TextButton.Name = Name
 		TextButton.Text = Name
 		TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -144,7 +149,7 @@ function Lib:CreateMain()
 		TextButton.TextWrapped = true
 	end
 	
-	return Combat
+	return Blatant
 end
 
 return Lib
